287a4cbf89c5f3e1460500bfc4d62c3aa9d089fc
TemirlanKulsarinov <temirlankulsarinov9@gmail.com>
2026-09-18 14:50:54 +0500

    full changes for js and html

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,19 +1,21 @@
+// Функция открытия раздела с резюме с главного экрана
+function showResumeSection() {
+    document.getElementById('welcomeScreen').style.display = 'none';
+    document.getElementById('resumeWrapper').classList.add('active-section');
+}
+
+// Функция переключения между участниками
 function switchTab(tabId, buttonElement) {
-    // 1. Скрываем все секции резюме
     const sections = document.querySelectorAll('.resume-section');
     sections.forEach(section => {
         section.classList.remove('active-tab');
     });
 
-    // 2. Убираем класс активности у всех кнопок навигации
     const buttons = document.querySelectorAll('.nav-btn');
     buttons.forEach(btn => {
         btn.classList.remove('active');
     });
 
-    // 3. Показываем ту секцию, на которую нажали
     document.getElementById(tabId).classList.add('active-tab');
-
-    // 4. Делаем нажатую кнопку активной (подсвечиваем)
     buttonElement.classList.add('active');
 }
